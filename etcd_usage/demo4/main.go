ef674cd4bfce17356d113bc00b55f0b25083e8e3
lin_genxu <m18328327481_1@163.com>
2022-05-16 21:39:05 +0800

    first

---
+++ etcd_usage/demo4/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go.etcd.io/etcd/api/v3/mvccpb"
+	clientv3 "go.etcd.io/etcd/client/v3"
+
+	"time"
+)
+
+func main() {
+	var (
+		cli     *clientv3.Client
+		err     error
+		kv      clientv3.KV
+		delResp *clientv3.DeleteResponse
+		kvPair  *mvccpb.KeyValue
+	)
+	cli, err = clientv3.New(clientv3.Config{
+		Endpoints:   []string{"192.168.0.16:2379"},
+		DialTimeout: time.Second * 5,
+	})
+	if err != nil {
+		return
+	}
+
+	defer cli.Close()
+
+	kv = clientv3.NewKV(cli)
+
+	if delResp, err = kv.Delete(context.TODO(), "/cron/jobs/", clientv3.WithPrefix()); err != nil {
+		fmt.Println(err)
+	}
+
+	if len(delResp.PrevKvs) != 0 {
+		for _, kvPair = range delResp.PrevKvs {
+			fmt.Println("delete:", string(kvPair.Key), string(kvPair.Value))
+		}
+	}
+}
